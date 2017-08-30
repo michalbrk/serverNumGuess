08740095bba4428a8ec0873f255aa777329dce96
Michał <brakowski.m@gmail.com>
2017-08-30 11:24:01 +0200

    deletion of the html and js files and add of the php file containing the whole app that is drawing random numbers

--- main.php
+++ main.php
@@ -1,18 +1,67 @@
+<html>
+    
+<title>serverNumGuess</title>
+    
+<body>
+<?php 
+    
+    function settingRanNum() {
+        
+    if(isset($_POST["number"]) == true) {
+
+        $ranNumb = mt_rand(1,100);
+        
+        if($_POST["number"] > $ranNumb) {
+            $resultTxt = 'Za duża liczba :('; 
+            
+        } else if($_POST["number"] < $ranNumb) {
+            $resultTxt = 'Za mała liczba :(';
+            
+        } else if($_POST["number"] == $ranNumb) {
+            $resultTxt = 'To ta liczba!!!';
+        
+        }
+            
+    echo "Twoja liczba to :  " . $_POST["number"]; 
+    echo "</br>"; 
+    echo "Wylosowana liczba to: " . $ranNumb;
+    echo "</br>";
+	echo $resultTxt;
+        
+    unset($_POST["number"]);
+        
+?>
+    
+<form action="main.php" method="post">
+    
+Podaj kolejną liczbę: <input type="text" name="number"><br>
+<input type="submit">
+    
+</form>
+    
+    
 <?php
-
-$firstArg = 1;
-$secondArg = 1000;
-
-function getRanNum($firstArg, $secondArg) {
+        
+    } else {
+        
+?>
     
-    global $firstArg;
-    global $secondArg;
+<form action="main.php" method="post">
+Twoja liczba to: <input type="text" name="number"><br>
+<input type="submit">
+</form>
     
-    $ranNumb = mt_rand($firstArg,$secondArg);
     
-    return $ranNumb;
+<?php
+        
+    }
 }
-
-echo(getRanNum($firstArg, $secondArg));
+  
+    
+echo settingRanNum();
 
 ?>
+    
+    
+</body>
+</html>
